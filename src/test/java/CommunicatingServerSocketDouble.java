871a9cdf55c40f731370329ff2dac747f9662f94
Gabba <gmedas@8thlight.com>
2018-05-08 16:25:26 +0100

    Reflects better the implementation of real socket.

--- src/test/java/CommunicatingServerSocketDouble.java
+++ src/test/java/CommunicatingServerSocketDouble.java
@@ -1,7 +1,26 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 public class CommunicatingServerSocketDouble implements ReadingSocket {
+
+    private final String messageFromClient;
+
+    public CommunicatingServerSocketDouble(String messageFromClient) {
+        this.messageFromClient = messageFromClient;
+    }
 
     @Override
     public String readStream() {
-        return "hello";
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input(messageFromClient)));
+        try {
+            return bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private InputStream input(String inputToRead) {
+        return new ByteArrayInputStream(inputToRead.getBytes(StandardCharsets.UTF_8));
     }
 }
--- src/test/java/CommunicatingServerTest.java
+++ src/test/java/CommunicatingServerTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CommunicatingServerTest {
@@ -16,17 +15,14 @@ public class CommunicatingServerTest {
     @Before
     public void newServer() {
         output = new ByteArrayOutputStream();
-        communicatingSocket = new CommunicatingServerSocketDouble();
+        communicatingSocket = new CommunicatingServerSocketDouble("hello");
         server = new CommunicatingServer(communicatingSocket, new ConsolePrinter(new PrintStream(output)));
     }
 
     @Test
     public void printsMessageReceivedFromClientToUser() {
-        String helloMessage = communicatingSocket.readStream();
-
         server.printClientMessage();
 
-        assertEquals(helloMessage, "hello");
         assertTrue(output.toString().contains("hello"));
     }
 }
